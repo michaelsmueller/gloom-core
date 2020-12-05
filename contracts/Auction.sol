c4cf8c6355b9ce0669d875d1d9dcb4bd9faecc54
Michael Mueller <hello@michaelmueller.dev>
2020-12-05 13:16:35 +0100

    remove unnecessary registerSeller function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -20,35 +20,34 @@ contract Auction {
     uint256 bid;
     uint256 bidDateTime;
   }
-
   mapping(address => Bidder) public bidders;
   address[] public bidderAddresses;
 
+  event ReceiveSellerDeposit(address indexed seller, uint indexed sellerDeposit);
+
   constructor(
+    address _seller,
     uint256 _tokenAmount,
     address _tokenContractAddress,
     uint256 _startDateTime,
     uint256 _endDateTime
   ) public {
     factory = msg.sender;
+    seller = _seller;
     tokenAmount = _tokenAmount;
     tokenContractAddress = _tokenContractAddress;
     startDateTime = _startDateTime;
     endDateTime = _endDateTime;
   }
 
-  function registerSeller(address _seller) external {
-    require(msg.sender == factory, 'Sender not authorized');
-    seller = _seller;
-  }
-
   function receiveSellerDeposit() external payable {
     // consider using initialize or other modifier to preven selling from changing deposit
     require(msg.sender == seller, 'Sender not authorized');
     sellerDeposit = msg.value;
+    emit ReceiveSellerDeposit(seller, sellerDeposit);
   }
 
-  function getBidders() public view returns (address[] memory) {
+  function getBidders() external view returns (address[] memory) {
     return bidderAddresses;
   }
 
--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -6,7 +6,7 @@ import './Auction.sol';
 // import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract AuctionFactory {
-  Auction[] public auctionAddresses;
+  Auction[] private auctionAddresses;
   address public admin;
 
   event AuctionCreated(Auction indexed auction, address indexed seller);
@@ -15,19 +15,13 @@ contract AuctionFactory {
     admin = msg.sender;
   }
 
-  function getAddresses() public view returns (Auction[] memory) {
+  function getAddresses() external view returns (Auction[] memory) {
     return auctionAddresses;
   }
 
-  function createAuction(
-    uint256 tokenAmount,
-    address tokenContractAddress,
-    uint256 startDateTime,
-    uint256 endDateTime,
-    address seller
-  ) public returns (address) {
-    Auction auction = new Auction(tokenAmount, tokenContractAddress, startDateTime, endDateTime);
-    auction.registerSeller(seller);
+  function createAuction(uint256 tokenAmount, address tokenContractAddress, uint256 startDateTime, uint256 endDateTime) external {
+    address seller = msg.sender;
+    Auction auction = new Auction(seller, tokenAmount, tokenContractAddress, startDateTime, endDateTime);
     auctionAddresses.push(auction);
     emit AuctionCreated(auction, seller);
   }
