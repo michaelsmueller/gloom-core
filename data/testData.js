daed4e32df966eee8a62fa5a38b3192d6b6c37a1
Michael Mueller <hello@michaelmueller.dev>
2020-12-19 00:48:12 +0100

    all tests pass

--- data/testData.js
+++ data/testData.js
@@ -1,5 +1,12 @@
+const BN = require('bn.js');
+
+const TOKENS = new BN(100);
+const DECIMALS = new BN(18);
+const TEN = new BN(10);
+const tokenAmount = TOKENS.mul(TEN.pow(DECIMALS));
+
 module.exports = {
-  tokenAmount: 100 * Math.pow(10, 18),
+  tokenAmount,
   tokenContractAddress: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', // YFI
   startDateTime: 1609488000000, // 1 Jan 2020 8:00 UTC
   endDateTime: 1612166400000, // 1 Feb 2020 8:00 UTC
