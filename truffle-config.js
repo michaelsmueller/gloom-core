f24280fc043a3407fc9cf839dd1dc43d935fa7ed
Michael Mueller <hello@michaelmueller.dev>
2020-12-22 03:22:10 +0100

    deployed to kovan

--- truffle-config.js
+++ truffle-config.js
@@ -16,42 +16,16 @@ module.exports = {
     },
     kovan: {
       provider: function () {
-        return new HDWalletProvider(
-          MNEMONIC,
-          // `https://kovan.infura.io/v3/${PROJECT_ID}`
-          `wss://kovan.infura.io/ws/v3/${PROJECT_ID}`,
-        );
+        return new HDWalletProvider(MNEMONIC, `wss://kovan.infura.io/ws/v3/${PROJECT_ID}`);
       },
-      // gas: 80000000,
       network_id: 42,
-      // networkCheckTimeout: 10000000000,
-    },
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${PROJECT_ID}`);
-      },
-      network_id: 3, // Ropsten's id
-      // provider: () => new HDWalletProvider(MNEMONIC, `http://localhost:8545`),
-      // gas: 5500000, // Ropsten has a lower block limit than mainnet
-      // confirmations: 0, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true,
-      networkCheckTimeout: 1000000,
     },
   },
   compilers: {
     solc: {
       version: '0.5.3',
-      // version: '0.6.2',
       parser: 'solcjs', // Leverages solc-js purely for speedy parsing
-      // settings: {
-      // optimizer: {
-      // enabled: true,
-      // runs: 1500,
-      // },
-      // evmVersion: 'istanbul',
       evmVersion: 'constantinople',
-      // },
     },
   },
 };
